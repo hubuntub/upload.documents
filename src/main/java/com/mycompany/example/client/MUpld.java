ff1bf5f49cc1d32c991449bf77f6f41b1a8ff83b
hubuntub <begona26@gmail.com>
2014-05-30 19:25:56 +0200

    finally

---
+++ src/main/java/com/mycompany/example/client/MUpld.java
@@ -0,0 +1,18 @@
+package com.mycompany.example.client;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+public class MUpld extends Composite {
+
+  private static MUpldUiBinder uiBinder = GWT.create(MUpldUiBinder.class);
+
+  interface MUpldUiBinder extends UiBinder<Widget, MUpld> {
+  }
+
+  public MUpld() {
+    initWidget(uiBinder.createAndBindUi(this));
+  }
+
+}
--- src/main/java/com/mycompany/example/client/MyGwtApp.java
+++ src/main/java/com/mycompany/example/client/MyGwtApp.java
@@ -1,17 +1,25 @@
 package com.mycompany.example.client;
 
+import gwtupload.client.IFileInput.FileInputType;
 import gwtupload.client.IUploadStatus.Status;
 import gwtupload.client.IUploader;
 import gwtupload.client.IUploader.UploadedInfo;
 import gwtupload.client.MultiUploader;
 import gwtupload.client.PreloadedImage;
 import gwtupload.client.PreloadedImage.OnLoadPreloadedImageHandler;
+import jsupload.client.ChismesUploadProgress;
+import jsupload.client.IncubatorUploadProgress;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Hidden;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
+import gwtupload.client.IDropZone;
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
@@ -32,15 +40,72 @@ public class MyGwtApp implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		  // Attach the image viewer to the document
+		// Attach the image viewer to the document
 	    RootPanel.get("thumbnails").add(panelImages);
 	    
 	    // Create a new uploader panel and attach it to the document
 	    MultiUploader defaultUploader = new MultiUploader();
 	    RootPanel.get("default").add(defaultUploader);
-
+	    
+	    
 	    // Add a finish handler which will load the image once the upload finishes
 	    defaultUploader.addOnFinishUploadHandler(onFinishUploaderHandler);
+	    defaultUploader.setMaximumFiles(3);
+	    defaultUploader.setFileInputPrefix("default");
+	    // You can add customized parameters to servlet call 
+	    defaultUploader.setServletPath(defaultUploader.getServletPath() + "?foo=bar");
+	    defaultUploader.avoidRepeatFiles(true);
+	    // This enables php apc progress mechanism
+	    defaultUploader.add(new Hidden("APC_UPLOAD_PROGRESS", defaultUploader.getInputName()));
+
+	    MultiUploader incubatorUploader = new MultiUploader(FileInputType.ANCHOR, new IncubatorUploadProgress());
+	    incubatorUploader.addOnFinishUploadHandler(onFinishUploaderHandler);
+	    incubatorUploader.setValidExtensions("jpg", "jpeg", "png", "gif");
+	    RootPanel.get("incubator").add(incubatorUploader);
+
+
+	    MultiUploader chismesUploader = new MultiUploader(FileInputType.BUTTON, new ChismesUploadProgress(false));
+	    chismesUploader.addOnFinishUploadHandler(onFinishUploaderHandler);
+	    RootPanel.get("chismes").add(chismesUploader);
+	    
+	    MUpld m = new MUpld();
+	    RootPanel.get("uibinder").add(m);
+
+	    MultiUploader dragDropUploader = new MultiUploader(FileInputType.DROPZONE);
+	    dragDropUploader.addOnFinishUploadHandler(onFinishUploaderHandler);
+	    RootPanel.get("dropzone").add(dragDropUploader);
+
+	    class DropZoneLabel extends Label implements IDropZone {
+	    }
+
+	    DropZoneLabel externalDropZone = new DropZoneLabel();
+	    externalDropZone.setText("");
+	    externalDropZone.setSize("180px", "40px");
+	    externalDropZone.getElement().getStyle().setBorderStyle(Style.BorderStyle.DASHED);
+	    externalDropZone.getElement().getStyle().setBorderWidth(1, Style.Unit.PX);
+	    externalDropZone.getElement().getStyle().setPadding(10, Unit.PX);
+
+	    MultiUploader externalDragDropUploader = new MultiUploader(FileInputType.DROPZONE.with((IDropZone) externalDropZone));
+	    externalDragDropUploader.addOnFinishUploadHandler(onFinishUploaderHandler);
+	    RootPanel.get("external_dropzone").add(externalDragDropUploader);
+	    RootPanel.get("external_dropzone").add(externalDropZone);
+
+	    MultiUploader customDragDropUploader = new MultiUploader(
+	            FileInputType.CUSTOM.with(new Label("Click me or drag and drop files on me")).with((IDropZone) null));
+	    customDragDropUploader.addOnFinishUploadHandler(onFinishUploaderHandler);
+	    RootPanel.get("custom_dropzone").add(customDragDropUploader);
+	    
+	    DropZoneLabel customExternalDropZone = new DropZoneLabel();
+	    customExternalDropZone.setText("Drop files here");
+	    customExternalDropZone.setSize("180px", "40px");
+	    customExternalDropZone.getElement().getStyle().setBorderStyle(Style.BorderStyle.DASHED);
+	    customExternalDropZone.getElement().getStyle().setBorderWidth(1, Style.Unit.PX);
+	    customExternalDropZone.getElement().getStyle().setPadding(10, Unit.PX);
+	    MultiUploader customExternalDragDropUploader = new MultiUploader(
+	            FileInputType.CUSTOM.with(new Button("Click me or drag and drop files below")).with((IDropZone) customExternalDropZone));
+	    customExternalDragDropUploader.addOnFinishUploadHandler(onFinishUploaderHandler);
+	    RootPanel.get("custom_external_dropzone").add(customExternalDragDropUploader);
+	    RootPanel.get("custom_external_dropzone").add(customExternalDropZone);
 	}
 	 // Load the image in the document and in the case of success attach it to the viewer
 	  private IUploader.OnFinishUploaderHandler onFinishUploaderHandler = new IUploader.OnFinishUploaderHandler() {
